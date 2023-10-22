8bd4a4f55a9f2ef5c8d4e0c44e65dd08cd6ee148
Veronika Kulieva <benikuben@yandex.ru>
2023-10-22 19:39:12 +0300

    Initial commit

---
+++ src/main/java/ru/neoflex/authvalidservice/AuthValidServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.authvalidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthValidServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthValidServiceApplication.class, args);
+    }
+
+}
